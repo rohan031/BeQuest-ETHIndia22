097b4357c6f4b7a3f0cdbc3bc32527b55bf978e9
Anshul Vats <anshulvatz@gmail.com>
2022-12-03 20:15:19 +0530

    client

--- client/src/backendConnectors/displayHelpers/displayBalance.js
+++
@@ -1,8 +0,0 @@
-import { ethers } from "ethers";
-
-const data = (num, decimal) => {
-	let x = ethers.utils.formatUnits(num, decimal);
-	return x;
-};
-
-export default data;
--- client/src/components/FetchingLoader.js
+++
@@ -1,3 +0,0 @@
-export default function FetchingLoader() {
-	return <span className="loader"></span>;
-}
